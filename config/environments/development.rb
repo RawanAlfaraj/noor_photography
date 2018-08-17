1984773e4b7a9232072678f745185aa73b81a131
Rawan <sk8r0rawan@gmail.com>
2018-08-16 20:58:07 -0400

    contact form error true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 # For mail_form gem
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors= false
+  config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',

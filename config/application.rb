e5b831ba3162e6518830ddbfbad6d9f814372e9c
Rawan <sk8r0rawan@gmail.com>
2018-08-21 03:55:47 -0400

    font

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module NoorPhotography
     # -- all .rb files in that directory are automatically loaded.
     config.assets.enabled = true
     config.assets.paths << Rails.root.join('/app/assets/fonts')
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
